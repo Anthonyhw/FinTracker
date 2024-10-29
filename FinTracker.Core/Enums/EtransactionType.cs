33638521031e89144c4f0b8840fddddbf6587438
Anthony <Anthony_dsant@outlook.com>
2024-10-28 22:10:16 -0300

    Add project files.

---
+++ FinTracker.Core/Enums/EtransactionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTracker.Core.Enums
+{
+    public enum EtransactionType
+    {
+        Deposit = 1,
+        Withdraw = 2,
+    }
+}
---
+++ FinTracker.Core/Models/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using FinTracker.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTracker.Core.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? PayedOrReceivedAt { get; set;}
+
+        public EtransactionType Type { get; set; } = EtransactionType.Withdraw;
+
+        public decimal Amount { get; set; }
+
+        public long CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+
+        public string UserId { get; set; } = string.Empty;
+
+    }
+}
