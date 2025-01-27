65159682c8554fac0f4ca3cead4eae76d0be3ba6
Anthony Dionisio dos Santos <anthony_dsant@outlook.com>
2025-01-27 15:19:52 -0300

    Criando testes unitários OrderHandler - CancelAsync

--- FinTracker.Tests/Configuration/DbInMemory.cs
+++ FinTracker.Tests/Configuration/DbInMemory.cs
@@ -35,26 +35,108 @@ namespace FinTracker.Tests.Configuration
         {
             if (_context.Database.EnsureCreated())
             {
-                _context.Vouchers.AddRange(
-                    new Voucher
-                    {
-                        Id = 1,
-                        Code = "1234",
-                        Amount = 150,
-                        Title = "Teste Ativo",
-                        Description = "Teste Ativo",
-                        IsActive = true,
-                    },
-                    new Voucher
-                    {
-                        Id = 2,
-                        Code = "5678",
-                        Amount = 200,
-                        Title = "Teste Inativo",
-                        Description = "Teste Inativo",
-                        IsActive = false,
-                    }
-                    );
+                var voucher1 = new Voucher
+                {
+                    Id = 1,
+                    Code = "1234",
+                    Amount = 150,
+                    Title = "Teste Ativo",
+                    Description = "Teste Ativo",
+                    IsActive = true,
+                };
+                var voucher2 = new Voucher
+                {
+                    Id = 2,
+                    Code = "5678",
+                    Amount = 200,
+                    Title = "Teste Inativo",
+                    Description = "Teste Inativo",
+                    IsActive = false,
+                };
+                _context.Vouchers.AddRange(voucher1, voucher2);
+
+                var product1 = new Product
+                {
+                    Id = 1,
+                    Title = "Produto teste 1",
+                    Description = "Produto teste 1",
+                    Slug = "teste-1",
+                    IsActive = true,
+                    Price = 250
+                };
+
+                var product2 = new Product
+                {
+                    Id = 2,
+                    Title = "Produto teste 2",
+                    Description = "Produto teste 2",
+                    Slug = "teste-2",
+                    IsActive = false,
+                    Price = 500
+                };
+                _context.Products.AddRange(product1, product2);
+
+                var order1 = new Order
+                {
+                    Id = 1,
+                    Code = "1234",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Product = product1,
+                    ProductId = product1.Id,
+                    Status = Core.Enums.EOrderStatus.Canceled,
+                    UserId = "userteste@hotmail.com",
+                    Voucher = voucher1,
+                    VoucherId = voucher1.Id,
+                    Gateway = Core.Enums.EPaymentGateway.Stripe
+                };
+
+                var order2 = new Order
+                {
+                    Id = 2,
+                    Code = "5678",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Product = product2,
+                    ProductId = product2.Id,
+                    Status = Core.Enums.EOrderStatus.WaitingPayment,
+                    UserId = "userteste@hotmail.com",
+                    Voucher = voucher2,
+                    VoucherId = voucher2.Id,
+                    Gateway = Core.Enums.EPaymentGateway.Stripe
+                };
+
+                var order3 = new Order
+                {
+                    Id = 3,
+                    Code = "9876",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Product = product2,
+                    ProductId = product2.Id,
+                    Status = Core.Enums.EOrderStatus.Paid,
+                    UserId = "userteste@hotmail.com",
+                    Voucher = voucher2,
+                    VoucherId = voucher2.Id,
+                    Gateway = Core.Enums.EPaymentGateway.Stripe
+                };
+
+                var order4 = new Order
+                {
+                    Id = 4,
+                    Code = "5432",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Product = product1,
+                    ProductId = product1.Id,
+                    Status = Core.Enums.EOrderStatus.Refunded,
+                    UserId = "userteste@hotmail.com",
+                    Voucher = voucher1,
+                    VoucherId = voucher1.Id,
+                    Gateway = Core.Enums.EPaymentGateway.Stripe
+                };
+
+                _context.Orders.AddRange(order1, order2, order3, order4);
 
                 _context.SaveChanges();
             }
---
+++ FinTracker.Tests/Handlers/OrderHandlerTests.cs
@@ -0,0 +1,117 @@
+﻿using FinTracker.Api.Data;
+using FinTracker.Api.Handlers;
+using FinTracker.Core.Enums;
+using FinTracker.Core.Handlers;
+using FinTracker.Core.Models;
+using FinTracker.Core.Requests.Orders;
+using FinTracker.Tests.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace FinTracker.Tests.Handlers
+{
+    [TestFixture]
+    public class OrderHandlerTests
+    {
+        private readonly OrderHandler orderHandler;
+
+        public OrderHandlerTests()
+        {
+            var dbInMemory = new DbInMemory();
+            var context = dbInMemory.GetContext();
+            var stripeHandler = new Mock<IStripeHandler>();
+            orderHandler = new OrderHandler(context, stripeHandler.Object);
+        }
+
+        [Test]
+        public async Task WhenCancellingOrder_IfOrderIsCanceled_ShouldError()
+        {
+            // Arrange
+            var request = new CancelOrderRequest() { Id = 1, UserId = "userteste@hotmail.com" };
+            
+
+            // Act
+            var result = await orderHandler.CancelAsync(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.That(result.Data, Is.Not.Null);
+            Assert.That(result.Code, Is.EqualTo(400));
+            Assert.That(result.Message, Is.EqualTo("Este pedido já foi cancelado."));
+
+        }
+
+        [Test]
+        public async Task WhenCancellingOrder_IfOrderIsPaid_ShouldError()
+        {
+            // Arrange
+            var request = new CancelOrderRequest() { Id = 3, UserId = "userteste@hotmail.com" };
+
+
+            // Act
+            var result = await orderHandler.CancelAsync(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.That(result.Data, Is.Not.Null);
+            Assert.That(result.Code, Is.EqualTo(400));
+            Assert.That(result.Message, Is.EqualTo("Este pedido já foi pago e não pode ser cancelado."));
+
+        }
+
+        [Test]
+        public async Task WhenCancellingOrder_IfOrderIsRefunded_ShouldError()
+        {
+            // Arrange
+            var request = new CancelOrderRequest() { Id = 4, UserId = "userteste@hotmail.com" };
+
+
+            // Act
+            var result = await orderHandler.CancelAsync(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.That(result.Data, Is.Not.Null);
+            Assert.That(result.Code, Is.EqualTo(400));
+            Assert.That(result.Message, Is.EqualTo("Este pedido já foi reembolsado."));
+
+        }
+
+        [Test]
+        public async Task WhenCancellingOrder_IfOrderIsWaitingPayment_ShouldSuccess()
+        {
+            // Arrange
+            var request = new CancelOrderRequest() { Id = 2, UserId = "userteste@hotmail.com" };
+
+
+            // Act
+            var result = await orderHandler.CancelAsync(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.That(result.Data, Is.Not.Null);
+            Assert.That(result.Code, Is.EqualTo(200));
+            Assert.That(result.Message, Is.EqualTo("Pedido cancelado com sucesso."));
+
+        }
+
+        [Test]
+        public async Task WhenCancellingOrder_IfOrderNotFound_ShouldError()
+        {
+            // Arrange
+            var request = new CancelOrderRequest() { Id = 8, UserId = "usernaoexiste@hotmail.com" };
+
+
+            // Act
+            var result = await orderHandler.CancelAsync(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.That(result.Data, Is.Null);
+            Assert.That(result.Code, Is.EqualTo(404));
+            Assert.That(result.Message, Is.EqualTo("Pedido não encontrado."));
+
+        }
+    }
+}
